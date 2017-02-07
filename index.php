9553bdd80f6794eab83a71706d6d09e1c571a874
KasaiDot <kasaidot@gmail.com>
2017-02-07 14:06:30 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+
+		<link rel=stylesheet href=".static/css/style.min.css?<?=microtime(true);?>">
+
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js?<?=microtime(true);?>"></script>
+	</head>
+	<body>
+		<h1></h1>
+
+		<script type="text/javascript" src=".static/js/script.min.js?<?=microtime(true);?>"></script>
+
+		<audio autobuffer>
+			<source src=".static/sound/sound.mp3?<?=microtime(true);?>">
+		</audio>
+	</body>
+</html>
